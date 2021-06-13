fc12a35b17ae58ed50742231b12bd88576d9ee47
Eskild Mageli <eskild.emedd33@gmail.com>
2021-06-13 11:06:10 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
 
 function App() {
+  const [message, setMessage] = useState(false)
+  const handleClick = () => {
+    setMessage(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <div className="form" >
+        <div style={{ width: "100%", margin: "1rem", display: "flex", justifyContent: "center" }}>
+
+          <Button type="primary" onClick={handleClick}>Is my data GDPR-compliant?</Button>
+        </div>
+        <div style={{ width: "100%", margin: "1rem", display: "flex", justifyContent: "center" }}>
+          {message ?
+            <h2 style={{ color: "red" }}>NO</h2>
+            : null
+          }
+        </div>
+      </div>
     </div>
   );
 }
